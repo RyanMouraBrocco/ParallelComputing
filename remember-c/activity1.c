ab029f1d325c6b551d7a5a56cc18a7a241027ab7
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2021-02-12 20:51:05 -0300

    added comments in activities

--- remember-c/activity1.c
+++ remember-c/activity1.c
@@ -1,3 +1,13 @@
+/*
+
+* Ryan Moura Brocco
+
+* 32175566
+
+* 1. Crie um programa capaz de ler os dados de uma matriz quadrada de inteiros. Ao final da leitura o programa deverá imprimir o número da linha que contém o menor dentre todos os números lidos.
+
+*/
+
 #include <stdio.h>
 
 void createArray(int array[2][2]);
@@ -11,12 +21,24 @@ void main()
     printf("The smaller number is in line: %d", smallerLineNumber);
 }
 
+/*
+inputs used:
+input 1 = | 1  2 |
+          | 3  4 |
+
+input 2 = | 5  5 |
+          | 5  5 |
+
+input 3 = | 5  6 |
+          | 3  4 |
+*/
+
 void createArray(int array[2][2])
 {
     array[0][0] = 5;
-    array[0][1] = 6;
-    array[1][0] = 3;
-    array[1][1] = 4;
+    array[0][1] = 5;
+    array[1][0] = 5;
+    array[1][1] = 5;
 }
 
 int getLineOfSmallerNumberInArray(int array[2][2])
--- remember-c/activity2.c
+++ remember-c/activity2.c
@@ -1,7 +1,40 @@
+/*
+
+* Ryan Moura Brocco
+
+* 32175566
+
+* 2. Escreva um programa que leia 3 notas de um aluno e a média das notas dos exercícios realizados por ele. Calcular a média de aproveitamento, usando a fórmula: MA = (N1 + N2*2 + N3*3 + ME)/7. A partir da média, informar o conceito de acordo com a tabela:
+        maior ou igual a 9	A
+        maior ou igual a 7.5 e menor que 9	B
+        maior ou igual a 6 e menor que 7.5	C
+        maior ou igual a 4 e menor que 6	D
+        menor que 4	E
+*/
+
 #include <stdio.h>
 
 double calculateAverage(double, double, double, double);
 char NumberNoteToLetterNote(double);
+
+/*
+    inputs used:
+    input 1: note1 = 10
+             note2 = 9
+             note3 = 7
+             noteExerciesAverange = 1
+
+    input 2: note1 = 6
+             note2 = 2
+             note3 = 4
+             noteExerciesAverange = 10
+
+    input 3: note1 = 4
+             note2 = 4
+             note3 = 1
+             noteExerciesAverange = 1
+
+*/
 
 void main()
 {
