30883d769cdcda74b534781bc24c446fd3193b33
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2021-02-14 15:38:27 -0300

    adjusted activity4 to show in correct Alphabetically order

--- remember-c/activity4.c
+++ remember-c/activity4.c
@@ -8,34 +8,33 @@
 
 */
 #include <stdio.h>
+#include "inputs.h"
 
 void printAlphabetically(char[60], char[60]);
 
 void main()
 {
-    char name1[60];
-    char name2[60];
-    printf("Write a name: ");
-    scanf("%s", &name1);
-    printf("Write a other name: ");
-    scanf("%s", &name2);
+    char name1[MAX_STR];
+    char name2[MAX_STR];
+    readString("Write a name", name1);
+    readString("Write a other name", name2);
     printAlphabetically(name1, name2);
 }
 
-void printAlphabetically(char name1[60], char name2[60])
+void printAlphabetically(char name1[MAX_STR], char name2[MAX_STR])
 {
-    for (int i = 0; i < 60; i++)
+    for (int i = 0; i < MAX_STR; i++)
     {
         if (name1[i] > name2[i])
         {
-            printf("%s\n", name1);
             printf("%s\n", name2);
+            printf("%s\n", name1);
             break;
         }
         else if (name1[i] < name2[i])
         {
-            printf("%s\n", name2);
             printf("%s\n", name1);
+            printf("%s\n", name2);
             break;
         }
     }
