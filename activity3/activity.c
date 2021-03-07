58fec4d7f46bc9c74aa5eaa8290e151f474d319d
RyanBrocco_linux <ryanmourabrocco@gmail.com>
2021-03-07 13:26:09 -0300

    added activity 3 to shared memory

--- activity3/activity.c
+++ activity3/activity.c
@@ -1,59 +1,60 @@
+/*
+
+* Ryan Moura Brocco
+
+* 32175566
+
+* 
+
+*/
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
+#include <unistd.h>
 
-#define SHMSZ 27
-
-main()
+void main()
 {
-    char c;
     int shmid;
     key_t key;
-    char *shm, *s;
+    int *shm, *value;
+    pid_t pid;
 
-    /*
-     * We'll name our shared memory segment
-     * "5678".
-     */
     key = 5678;
 
-    /*
-     * Create the segment.
-     */
-    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0)
+    if ((shmid = shmget(key, sizeof(int), IPC_CREAT | 0666)) < 0)
     {
-        perror("shmget");
-        exit(1);
+        printf("error when create segment");
+        return;
     }
 
-    /*
-     * Now we attach the segment to our data space.
-     */
-    if ((shm = shmat(shmid, NULL, 0)) == (char *)-1)
+    if ((shm = shmat(shmid, NULL, 0)) == (int *)-1)
     {
-        perror("shmat");
-        exit(1);
+        printf("error to allocate a variable");
+        return;
+    }
+    value = shm;
+    *value = 1;
+    printf("Initial value: %d\n", *value);
+
+    pid = fork();
+    if (pid == -1)
+    {
+        printf("Error to create fork");
+        return;
     }
 
-    /*
-     * Now put some things into the memory for the
-     * other process to read.
-     */
-    s = shm;
+    if (pid == 0)
+    {
+        *value += 2;
+    }
+    else
+    {
+        while (*value == 1)
+            ;
 
-    for (c = 'a'; c <= 'z'; c++)
-        *s++ = c;
-    *s = NULL;
-
-    /*
-     * Finally, we wait until the other process 
-     * changes the first character of our memory
-     * to '*', indicating that it has read what 
-     * we put there.
-     */
-    while (*shm != '*')
-        sleep(1);
-
-    exit(0);
+        *value *= 4;
+        printf("Final value: %d\n", *value);
+    }
 }
